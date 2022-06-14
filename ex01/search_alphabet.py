2f4db706b6aec01cdfe388d3d616f585ad22f1f9
HIRO <c0a2113899@edu.teu.ac.jp>
2022-06-14 18:00:10 +0900

    アルファベットゲーム

---
+++ ex01/search_alphabet.py
@@ -0,0 +1,42 @@
+import random
+import time
+
+max_question_num = 10
+target_num = 7 # 対象文字数
+miss_num = 2   # 欠損文字数
+
+def main():
+    start_time = time.time() # 時間の計測を開始する
+    for i in range(max_question_num):
+        ans = search_alphabet()
+        check = response(ans)
+        if check == 1:
+            break
+    end_time = time.time() # 時間の計測を終了する
+    elapsed_time = end_time - start_time # 経過時間の計算
+
+def search_alphabet():
+    alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    all_alphabet = random.sample(alphabet, target_num)
+    print(f"対象文字：{all_alphabet}")
+
+    miss_alphabet = random.sample(all_alphabet, miss_num)
+    print(f"表示文字：{all_alphabet - miss_alphabet}")
+
+def response(ans):
+    response_num = int(input("欠損文字数を答えてください。"))
+    if response_num == miss_num:
+        print("あたりです。では欠損文字を1つずつ答えてください。")
+        for i in range(miss_num):
+            response_alphabet = input(f"{i+1}つ目の文字を答えてください。")
+            if response_alphabet != ans:
+                print("…違います。最初からやり直してください。")
+                print("-----------------------------------------------")
+                return 0
+            ans.remove(response_num)
+        return 1
+    else:
+        print("…違います。")
+        return 0
+
+main()
