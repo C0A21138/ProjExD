c2a67470a4c95c85b3de8806c36f3f3e555475ca
HIRO <c0a2113899@edu.teu.ac.jp>
2022-06-21 14:34:57 +0900

    問1

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,7 +5,18 @@ root = tk.Tk() # tkモジュールにあるTkのインスタンスを作成
 root.title("calculator") # タイトルに"calculator"を表示
 root.geometry("300x500") # 300x500のウィンドウを作成
 
+
+
+
 root.mainloop() # ウィンドウを表示する
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
+    r, c = 0, 0
+    for num in range(9, -1, -1):
+        btn = tk.Button(root,
+                        text = f"{num}",
+                        width = 4,
+                        height = 2,
+                        font = ("Times New Roman", 30)
+                    )
