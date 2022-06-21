54945e7b40372f6fb3e2281194337debd4682bc9
HIRO <c0a2113899@edu.teu.ac.jp>
2022-06-21 14:11:28 +0900

    問1

---
+++ ex02/calc.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+root = tk.Tk() # tkモジュールにあるTkのインスタンスを作成
+root.title("calculator") # タイトルに"calculator"を表示
+root.geometry("300x500") # 300x500のウィンドウを作成
+
+root.mainloop() # ウィンドウを表示する
+
+
+#if __name__ == "__main__":
