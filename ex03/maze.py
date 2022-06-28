d51873f473e3a6b2a5360ea1399465954485d8d6
HIRO <c0a2113899@edu.teu.ac.jp>
2022-06-28 14:28:13 +0900

    問3

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,8 +4,14 @@ import tkinter as tk
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root = tk.Tk() # tkモジュールにあるTkのインスタンスを作成
+    
+    root.title("迷えるこうかとん") # titleを"迷えるこうかとん"に設定
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+
+    tori = tk.PhotoImage(file="fig0/.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
+
     canvas.pack()
     root.mainloop()
