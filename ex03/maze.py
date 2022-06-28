eab868493b7f01772a8fc0a364dc37e878981971
HIRO <c0a2113899@edu.teu.ac.jp>
2022-06-28 17:47:12 +0900

    コメント追加 #8

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,7 @@
 import tkinter as tk # tkinterモジュールをtkとして読み込む
 import maze_maker as mm # maze_makerモジュールをmmとして読み込む
 import tkinter.messagebox as tkm # tkinterモジュール内にあるmessageboxモジュールをtkmとして読み込む
-from random import randint
+from random import randint # randomモジュール内にある関数randintを読み込む
 
 def key_down(event): # キーが押された時に処理を行う関数を定義
     global key # グローバル宣言
@@ -32,8 +32,8 @@ def main_proc():
         count += 1 # 移動回数を1増やす
     
     elif count == 50: # 移動回数が50回になったとき
-        tkm.showerror("脱出失敗", "脱出に失敗しました…")
-        quit()
+        tkm.showerror("脱出失敗", "脱出に失敗しました…") # タイトルが脱出失敗のウインドウ内に"脱出に失敗しました…"を表示
+        quit() # ゲーム終了(プログラムを終了)
 
     entry.delete(0, tk.END)
     entry.insert(tk.END, count)
@@ -43,8 +43,8 @@ def main_proc():
 
     if mx == g_x and my == g_y:
         mx, my = cx, cy
-        tkm.showinfo("脱出成功", "脱出に成功しました！")
-        quit()
+        tkm.showinfo("脱出成功", "脱出に成功しました！") # タイトルが脱出成功のウインドウ内に"脱出に成功しました！"を表示
+        quit() # ゲーム終了(プログラムを終了)
 
 if __name__ == "__main__":
     maze_x = 15 # 横：15
@@ -54,8 +54,8 @@ if __name__ == "__main__":
 
     count = 0 # 移動回数を0で初期化
 
-    entry = tk.Entry(root, width = 100, text = count, font = ("Times New Roman", 40))
-    entry.pack()
+    entry = tk.Entry(root, width = 100, text = count, font = ("Times New Roman", 40)) # 上に移動回数を表示するentryを表示
+    entry.pack() # entryを表示する
 
     root.title("迷えるこうかとん") # titleを"迷えるこうかとん"に設定
     canvas = tk.Canvas(root, width=1500, height=900, bg="black") # 幅1500、高さ900、背景色"black"のcanvasを作成
@@ -64,14 +64,15 @@ if __name__ == "__main__":
     maze_bg = mm.make_maze(maze_x, maze_y) # 1：壁 / 0：床を表す二次元リスト
     mm.show_maze(canvas, maze_bg) # canvasにmaze_bgを描く
 
-    canvas.create_rectangle(100, 100, 200, 200, fill = "lightgreen")
+    canvas.create_rectangle(100, 100, 200, 200, fill = "lightgreen") # スタート地点の色を"lightgreen"に変更
 
-    g_x = randint(1, maze_x)
-    g_y = randint(1, maze_y)
-    while maze_bg[g_y][g_x] != 0 or (g_x == 1 and g_y == 1):
+    g_x = randint(1, maze_x) # ゴール地点のx座標をランダムに設定
+    g_y = randint(1, maze_y) # ゴール地点のy座標をランダムに設定
+    while maze_bg[g_y][g_x] != 0 or (g_x == 1 and g_y == 1): # ランダムに設定した座標先が床じゃない、または、スタート地点の場合
+        # 再度ゴール地点の座標を設定
         g_x = randint(1, maze_x-1)
         g_y = randint(1, maze_y-1)
-    canvas.create_rectangle(g_x*100, g_y*100, g_x*100+100, g_y*100+100, fill = "yellow")
+    canvas.create_rectangle(g_x*100, g_y*100, g_x*100+100, g_y*100+100, fill = "yellow") # ゴール地点の色を"yellow"に設定し作成
 
     tori = tk.PhotoImage(file="fig/0.png") # figフォルダにある画像「0.png」を読み込み、インスタンスを生成
     mx, my = 1, 1 # 変数mx、myをそれぞれ1で初期化
