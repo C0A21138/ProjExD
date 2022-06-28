264d65f6d476cc81b017cd215bd60389b8ac875d
HIRO <c0a2113899@edu.teu.ac.jp>
2022-06-28 17:07:30 +0900

    3限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,7 @@
 import tkinter as tk # tkinterモジュールをtkとして読み込む
 import maze_maker as mm # maze_makerモジュールをmmとして読み込む
+import tkinter.messagebox as tkm # tkinterモジュール内にあるmessageboxモジュールをtkmとして読み込む
+from random import randint
 
 def key_down(event): # キーが押された時に処理を行う関数を定義
     global key # グローバル宣言
@@ -12,33 +14,64 @@ def key_up(event): # キーが離された時に処理を行う関数を定義
 
 
 def main_proc():
-    global cx, cy, mx, my # グローバル宣言
+    global cx, cy, mx, my, count # グローバル宣言
     if key == "Up" and maze_bg[my-1][mx] == 0: # 押されたキーが「Up」 かつ　移動先が床のとき
         my -= 1 # 上に1マス移動する
+        count += 1 # 移動回数を1増やす
         
     elif key == "Down" and maze_bg[my+1][mx] == 0: # 押されたキーが「Down」 かつ　移動先が床のとき
         my += 1 # 下に1マス移動する
+        count += 1 # 移動回数を1増やす
 
     elif key == "Left" and maze_bg[my][mx-1] == 0: # 押されたキーが「Left」 かつ　移動先が床のとき
         mx -= 1 # 左に1マス移動する
+        count += 1 # 移動回数を1増やす
 
     elif key == "Right" and maze_bg[my][mx+1] == 0: # 押されたキーが「Right」 かつ　移動先が床のとき
         mx += 1 # 右に1マス移動する
+        count += 1 # 移動回数を1増やす
+    
+    elif count == 50: # 移動回数が50回になったとき
+        tkm.showerror("脱出失敗", "脱出に失敗しました…")
+        quit()
 
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, count)
     cx, cy = mx*100+50, my*100+50 # 移動幅を1マスに設定
     canvas.coords("tori", cx, cy) # こうかとんの座標を更新する
     root.after(100, main_proc) # 100ミリ秒後に関数main_proc()を呼び出す
 
+    if mx == g_x and my == g_y:
+        mx, my = g_x, g_y
+        tkm.showinfo("脱出成功", "脱出に成功しました！")
+        quit()
+
 if __name__ == "__main__":
+    maze_x = 15 # 横：15
+    maze_y = 9 # 縦：9
+
     root = tk.Tk() # tkモジュールにあるTkのインスタンスを作成
+
+    count = 0 # 移動回数を0で初期化
+
+    entry = tk.Entry(root, width = 100, text = count, font = ("Times New Roman", 40))
+    entry.pack()
 
     root.title("迷えるこうかとん") # titleを"迷えるこうかとん"に設定
     canvas = tk.Canvas(root, width=1500, height=900, bg="black") # 幅1500、高さ900、背景色"black"のcanvasを作成
     canvas.pack() # canvasを表示
 
-    maze_bg = mm.make_maze(15, 9) # 1：壁 / 0：床を表す二次元リスト
-                                  # 横：15、縦：9
+    maze_bg = mm.make_maze(maze_x, maze_y) # 1：壁 / 0：床を表す二次元リスト
     mm.show_maze(canvas, maze_bg) # canvasにmaze_bgを描く
+
+    canvas.create_rectangle(100, 100, 200, 200, fill = "lightgreen")
+
+    g_x = randint(1, maze_x)
+    g_y = randint(1, maze_y)
+    while maze_bg[g_y][g_x] != 0:
+        g_x = randint(1, maze_x-1)
+        g_y = randint(1, maze_y-1)
+    canvas.create_rectangle(g_x*100, g_y*100, g_x*100+100, g_y*100+100, fill = "yellow")
 
     tori = tk.PhotoImage(file="fig/0.png") # figフォルダにある画像「0.png」を読み込み、インスタンスを生成
     mx, my = 1, 1 # 変数mx、myをそれぞれ1で初期化
@@ -50,5 +83,7 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up) # キーが離されたとき、関数key_up()を呼び出す
 
     main_proc() # 関数main_proc()を呼び出す
+
+    #tkm.showinfo("脱出開始", "50回歩くまでに脱出しよう!")
 
     root.mainloop() # ウィンドウを表示する
