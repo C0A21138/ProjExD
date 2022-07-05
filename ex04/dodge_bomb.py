4186b9898cce1833ead0fcdf35ad92c0c2fafcde
HIRO <c0a2113899@edu.teu.ac.jp>
2022-07-05 14:24:03 +0900

    問3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,16 +3,33 @@ import sys
 
 def main():
     clock = pg.time.Clock()
-
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900)) # Surface
     screen_rect = screen_sfc.get_rect()           # Rect
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")    # Surface
     bgimg_rect = bgimg_sfc.get_rect()             # Rect
-    screen_sfc.blit(bgimg_sfc, bgimg_rect)        # Screen_sfc Surfaceにbgimg_sfc Surfaceを bgimg_rectに従って貼り付ける
+
+    # Screen_sfc Surfaceにbgimg_sfc Surfaceを bgimg_rectに従って貼り付ける
+    screen_sfc.blit(bgimg_sfc, bgimg_rect)
+
+    toriimg_sfc = pg.image.load("fig/6.png")                  # Surface
+    toriimg_sfc = pg.transform.rotozoom(toriimg_sfc, 0, 2.0)  # Surface
+    toriimg_rect = toriimg_sfc.get_rect()                     # Rect
     
-    #pg.display.update()
-    clock.tick(0.5)
+    # x座標900, y座標400の位置にこうかとんを表示する
+    toriimg_rect.center = 900, 400
+
+
+    while True:
+        screen_sfc.blit(bgimg_sfc, bgimg_rect)      # 背景
+        screen_sfc.blit(toriimg_sfc, toriimg_rect)  # こうかとん
+
+        for event in pg.event.get():  # イベントを繰り返して処理
+            if event.type == pg.QUIT: # ウィンドウのXボタンをクリックしたら
+                return
+            
+        pg.display.update()
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
